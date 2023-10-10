9f36f7a91e4d63fb893d55a0d1bdf1f498d233f4
Calvin Joy Baroi <cjoy720@Calvins-MacBook-Pro.local>
2023-10-10 16:49:39 +0200

    Added the subject and a Folder named Function Needed

---
+++ function_needed/ft_isalnum.c
@@ -0,0 +1,23 @@
+int	ft_isalnum(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z'
+			|| str[i] >= 'a' && str[i] <= 'z')
+			i++;
+		else
+			return (0);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
