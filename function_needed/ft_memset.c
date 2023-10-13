a3e043ee9b18457597d658ccee36ad34c69db0c5
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-13 23:32:32 +0200

    Added ft_memset.c

--- function_needed/ft_memset.c
+++ function_needed/ft_memset.c
@@ -3,26 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cjoy720 <cjoy720@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 21:16:55 by cjoy720           #+#    #+#             */
-/*   Updated: 2023/10/11 21:17:13 by cjoy720          ###   ########.fr       */
+/*   Updated: 2023/10/13 23:32:18 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *b, int c, size_t len)
+#include <stddef.h>
+
+void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*byte_ptr;
 	unsigned char	byte_value;
 
 	byte_value = (unsigned char) c;
-	byte_ptr = (unsigned char *) b;
+	byte_ptr = (unsigned char *) s;
 	i = 0;
-	while (i < len)
+	while (i < n)
 	{
 		byte_ptr[i] = byte_value;
 		i++;
 	}
-	return (b);
+	return (s);
 }
