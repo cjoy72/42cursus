bae0c4695d8cd3c51bb93f33a35333d969c62817
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-14 12:31:07 +0200

    Added ft_memcpy.c

--- function_needed/ft_memcpy.c
+++ function_needed/ft_memcpy.c
@@ -6,11 +6,11 @@
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 21:07:46 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/12 21:46:11 by cbaroi           ###   ########.fr       */
+/*   Updated: 2023/10/14 12:30:46 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <stddef.h>
 
 void	*memcpy(void *dest, const void *src, size_t n)
 {
