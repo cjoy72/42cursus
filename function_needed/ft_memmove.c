76acde625ff843d8b959847f4d0821a1de995277
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-16 16:53:34 +0200

    Added memmove

--- function_needed/ft_memmove.c
+++ function_needed/ft_memmove.c
@@ -6,24 +6,36 @@
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 17:58:29 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/12 18:30:58 by cbaroi           ###   ########.fr       */
+/*   Updated: 2023/10/16 16:37:31 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdlib.h>
+#include <stdlib.h>
 
 void	*memmove(void *dest, const void *src, size_t n)
 {
-	char	*s;
-	char	*d;
 	size_t	i;
+	char	*d;
+	char	*s;
 
-	s = (char *)src;
-	d = (char *)dest;
-	i = 0;
-	while (i <= n)
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (s < d && s + n > d)
 	{
-		d[i] = s[i];
-		i++;
+		while (n > 0)
+		{
+			d[n - 1] = s[n - 1];
+			n--;
+		}
 	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
 }
