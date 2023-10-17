b88fb531d02b47e1caf382262f45fc6bdd1d4586
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-17 09:42:23 +0200

    Added ft_toupper.c

--- function_needed/ft_toupper.c
+++ function_needed/ft_toupper.c
@@ -1,17 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/17 09:34:32 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/17 09:40:55 by cbaroi           ###   ########.fr       */
+/*   Created: 2023/10/17 09:28:23 by cbaroi            #+#    #+#             */
+/*   Updated: 2023/10/17 09:42:02 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include <stdio.h>
 
 int	toupper(int c)
 {
@@ -19,18 +18,4 @@ int	toupper(int c)
 		return (c - 32);
 	else
 		return (c);
-}
-
-int main()
-{
-    char    c;
-    c = 'g';
-    printf("given %c\n", c);
-    printf("return %c\n", toupper(c));
-    c = 'A';
-    printf("given %c\n", c);
-    printf("return %c\n", toupper(c));
-    c = ']';
-    printf("given %c\n", c);
-    printf("return %c\n", toupper(c));
 }
