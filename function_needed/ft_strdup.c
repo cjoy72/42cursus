bd2bd84a0bc310f85ecd4c755c7af6be8a68128c
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-18 16:09:48 +0200

    Added ft_strdup.c

---
+++ function_needed/ft_strdup.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 15:49:58 by cbaroi            #+#    #+#             */
+/*   Updated: 2023/10/18 16:08:33 by cbaroi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	int		s1_len;
+	int		i;
+
+	s1_len = ft_strlen(s1);
+	s2 = (char *)malloc(s1_len + 1);
+	if (s2 == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
