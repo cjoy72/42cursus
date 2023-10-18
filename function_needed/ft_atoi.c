10d96895bb827647de3d0f9e43538b1df2a20de5
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-18 13:20:12 +0200

    Added ft_atoi.c

---
+++ function_needed/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 12:37:13 by cbaroi            #+#    #+#             */
+/*   Updated: 2023/10/18 13:19:05 by cbaroi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isspace(char *c)
+{
+	if (c == 32 || (c >= '\t' && c <= '\r'))
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	n;
+	int	sign;
+
+	sign = 1;
+	while (ft_isspace(*nptr) == 1 || *nptr == '+')
+		nptr++;
+	if (*nptr >= 'a' && *nptr <= 'z'
+		|| *nptr >= 'A' && *nptr <= 'Z')
+		return (0);
+	if (*nptr == '-')
+	{
+		sign = -1;
+		nptr++;
+	}
+	n = 0;
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		n *= 10;
+		n += (*nptr - '0');
+		nptr++;
+	}
+	return (n * sign);
+}
