3f3574c014ee4303bc9fb929ab0c67873ca8167a
cjoy72 <140074376+cjoy72@users.noreply.github.com>
2023-10-19 15:35:42 +0200

    finished ft_strtrim.c

--- function_needed/ft_strtrim.c
+++ function_needed/ft_strtrim.c
@@ -6,7 +6,64 @@
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:37:18 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/19 14:37:21 by cbaroi           ###   ########.fr       */
+/*   Updated: 2023/10/19 15:24:48 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_strcharcmp(char *set, char c)
+{
+	int	i;
+	int	len;
+
+	len = ft_strlen(set);
+	i = 0;
+	while (i < len)
+	{
+		if (set[i] == c)
+			return (1);
+		else
+			i++;
+	}
+	return (0);
+}
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		i;
+	char	*ls;
+	int		size;
+	char	*str;
+
+	i = 0;
+	while (ft_strcharcmp(set, &s1[i]))
+		i++;
+	ls = (s1 + i);
+	size = ft_strlen(ls)
+	i = size - 1;
+	while (ft_strcharcmp(set, &ls[i]))
+		i--;
+	i++;
+	str = (char *)malloc(sizeof(char) * i);
+	if (str == NULL)
+		return (NULL);
+	size = i;
+	i = 0;
+	while (i < size)
+	{
+		str[i] = ls[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
