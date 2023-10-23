086679cb314fc577a4946fb97f9e0d712bf87922
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-23 17:40:45 +0200

    Added ft_lstadd_back.c

--- Bonus/ft_lstadd_back.c
+++ Bonus/ft_lstadd_back.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/23 11:07:32 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/23 17:28:29 by cbaroi           ###   ########.fr       */
+/*   Created: 2023/10/23 17:00:50 by cbaroi            #+#    #+#             */
+/*   Updated: 2023/10/23 17:40:22 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,23 @@
 typedef struct s_list
 {
 	void			*content;
-	struct s_list	*next;
+	struct s_list	**next;
 }					t_list;
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (lst == NULL)
-		return (NULL);
-	if (new != NULL)
+	t_list	*tmp;
+
+	if (lst == NULL || new == NULL)
+		return ;
+	if (*lst == NULL)
+		*lst = new ;
+	else
 	{
-		new->next = *lst;
-		(*lst) = new;
+		tmp = *lst;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
 	}
+	new->next = NULL;
 }
