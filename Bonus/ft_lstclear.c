1f15ad586d561c6c42eb771cc511a4ba940eb727
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-23 18:07:31 +0200

    added lstclear.c

---
+++ Bonus/ft_lstclear.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 17:48:28 by cbaroi            #+#    #+#             */
+/*   Updated: 2023/10/23 18:03:48 by cbaroi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+void	ft_lstdelone(t_list *lst, void (*del) (void *))
+{
+	del(lst->content);
+	free(lst);
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	if (*lst == NULL || del == NULL || lst == NULL)
+		return ;
+	if ((*lst)->next != NULL)
+		ft_clear(&((*lst)->next), del);
+	ft_lstdelone((*lst), (*del));
+	free(lst);
+}
