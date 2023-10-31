ba0af1c28dad0e4392d2ef054c1779d5b358bc8f
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-31 14:34:15 +0100

    asdf

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,22 +6,16 @@
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:07:32 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/23 17:28:29 by cbaroi           ###   ########.fr       */
+/*   Updated: 2023/10/31 14:27:00 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
+#include "libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (lst == NULL)
-		return (NULL);
+		return ;
 	if (new != NULL)
 	{
 		new->next = *lst;
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,15 +6,11 @@
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 17:43:10 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/23 17:46:44 by cbaroi           ###   ########.fr       */
+/*   Updated: 2023/10/31 14:33:42 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
+#include "libft.h"
 
 void	ft_lstdelone(t_list *lst, void (*del) (void *))
 {
