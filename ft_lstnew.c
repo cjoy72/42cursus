965aaa818f84f600bd08bb9572ac4160f9f12925
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-31 14:44:40 +0100

    modified

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,17 +6,11 @@
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 00:22:17 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/23 10:20:04 by cbaroi           ###   ########.fr       */
+/*   Updated: 2023/10/31 14:35:41 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
+#include "libft.h"
 
 t_list	*ft_lstnew(void *content)
 {
@@ -25,4 +19,5 @@ t_list	*ft_lstnew(void *content)
 	new = (t_list *)malloc(sizeof(t_list));
 	new->content = content;
 	new->next = NULL;
+	return (new);
 }
--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,24 +6,18 @@
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:24:18 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/23 13:30:54 by cbaroi           ###   ########.fr       */
+/*   Updated: 2023/10/31 14:39:47 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
+#include "libft.h"
 
 int	ft_lstsize(t_list *lst)
 {
 	int	i;
 
 	if (lst == NULL)
-		return (NULL);
+		return (0);
 	i = 1;
 	while (lst->next != NULL)
 	{
