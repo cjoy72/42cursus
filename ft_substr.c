c3bae1d7a01984df62e2c165317a46886d8cec90
Calvin Joy Baroi <cjoy720@gmail.com>
2023-10-31 21:39:23 +0100

    ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,29 +6,35 @@
 /*   By: cbaroi <cbaroi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 12:20:50 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/10/31 14:18:07 by cbaroi           ###   ########.fr       */
+/*   Updated: 2023/10/31 21:39:03 by cbaroi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	char			*str;
-	unsigned int	i;
-	char			*substr;
+	char	*substr;
+	size_t	i;
 
-	str = (char *)s;
-	substr = (char *)malloc(sizeof(char) * (len + 1));
+	i = -1;
+	if (start > ft_strlen(s))
+	{
+		substr = malloc(sizeof(char) * 1);
+		if (substr == NULL)
+			return (NULL);
+		*substr = '\0';
+		return (substr);
+	}
+	s += start;
+	if (ft_strlen(s) <= len)
+		substr = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	else
+		substr = malloc(sizeof(char) * (len + 1));
 	if (substr == NULL)
 		return (NULL);
-	i = 0;
-	while (str[start] != '\0' && i < len)
-	{
-		substr[i] = str[start];
-		start++;
-		i++;
-	}
+	while (s[++i] != '\0' && i < len)
+		substr[i] = s[i];
 	substr[i] = '\0';
 	return (substr);
 }
